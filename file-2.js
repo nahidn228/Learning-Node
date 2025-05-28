1c25f5b014dceaab329d994b005d79132af6c33d
nahidn228 <nahidn228@gmail.com>
2025-05-28 12:09:23 +0600

    Explore commonjs module

---
+++ file-2.js
@@ -0,0 +1,4 @@
+const { a, add } = require("./file1");
+
+console.log(a);
+console.log(add(3, 2));
---
+++ file1.js
@@ -0,0 +1,5 @@
+const a = 20;
+const add = (param1, param2) => param1 + param2;
+
+// console.log(module);
+module.exports = { a, add };
