13308ab8c3642cfd323145b47ec45903a49112be
nahidn228 <nahidn228@gmail.com>
2025-06-14 21:02:59 +0600

    create a simple logger app

---
+++ logger-app/index.js
@@ -0,0 +1,18 @@
+const path = require("path");
+const fs = require("fs");
+
+const inputArguments = process.argv.slice(2);
+const text = inputArguments.join(" ");
+
+const message = `${text} \n`;
+
+if (!message) {
+  console.log("❌ Please provide a message to log");
+  console.log("Example: node index.js Hello world");
+  process.exit(1);
+}
+
+const filePath = path.join(__dirname, "log.txt");
+fs.appendFile(filePath, message, { encoding: "utf-8" }, () => {
+  console.log("Your Log added successfully");
+});
