bae4fab1e8e8ceaab0e8ba0d746624da55418a36
nahidn228 <nahidn228@gmail.com>
2025-06-14 21:49:41 +0600

    explore Routing in node js

--- ToDo-App/server.js
+++ ToDo-App/server.js
@@ -1,6 +1,14 @@
 const http = require("http");
 const server = http.createServer((req, res) => {
-  res.end("Welcome to ToDo-App Server");
+  // res.end("Welcome to ToDo-App Server");
+
+  if (req.url === "/todos" && req.method === "GET") {
+    res.end("ALL ToDos here");
+  } else if (req.url === "/todos/create-todo" && req.method === "POST") {
+    res.end(" ToDos created");
+  } else {
+    res.end("Route not Found");
+  }
 });
 
 server.listen(5000, "127.0.0.1", () => {
